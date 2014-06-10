2b0facb7d8247b5e408d19847500b72851f7d56e
Ei Wai Wai Thein <eiwaiwai.thein@SIN-EIWAIWAIT.local>
2014-06-10 11:55:23 +0800

    added typealias

---
+++ 06_typeAliases.playground/section-1.swift
@@ -0,0 +1,21 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+// “Type aliases define an alternative name for an existing type. You define type aliases with the typealias keyword.”
+
+//typealias
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+//booleans
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious{
+    println("Mmm, tasty turnips!")
+}
+else{
+    println("Eww, turnips are horrible.")
+}
+
